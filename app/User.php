31bea327757ec3491c27d6a2c5e8823bd82f6cbf
lffruhling <lffruhling@hotmail.com>
2019-12-09 18:57:06 -0300

    Segunda alteração

--- app/User.php
+++ app/User.php
@@ -28,6 +28,6 @@ class User extends Authenticatable
     ];
 
     public function comentario(){
-        return $this->hasMany(Comentario: :class);
+        return $this->hasMany(Comentario:: class);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')-> insert([
+        	'name' => 'admin',
+        	'email' => 'admin@admin.com',
+        	'tipo' => 1,
+        	'password' => bcrypt('admin'),
+        ]);
     }
 }
